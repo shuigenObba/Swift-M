77f9770429bec7b497a89a8a1c962200aed8503e
huangshuigen <huangshuigen@sefon.com>
2017-11-17 10:10:49 +0800

    新增字符串自适应高度

--- Example/Swift-M/ViewController.swift
+++ Example/Swift-M/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        let string = "啊看上大SD卡是肯定卡结婚就ad黄金卡是递减哈时间肯定哈客户端撒谎 话就肯定会极爱的哈抠脚大汉金卡是递减阿莎黑豆浆卡上道具卡是递减卡仕达家山东矿机爱神的箭看哈市健康的哈时间肯定会撒娇蝴蝶卡是递减撒谎就卡上的框架阿时间肯定哈市框架搭建企划书"
+        let subString = "asdqwedsa爱神的箭阿莎啊就是打哈哈啊还是觉得哈炬华科技和大环境阿莎觉得可ad花见花开"
+        print("自适应的高度为\(string.autoLabelHeight(with: string, labelWidth: 200, attributes: [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 17)]))")
+        print("自适应的高度为\(subString.autoLabelHeight(with: subString, labelWidth: 200, attributes: [NSAttributedStringKey.font:UIFont.systemFont(ofSize: 17)]))")
+
         
         
     }
--- Swift-M/Classes/String+Extension.swift
+++ Swift-M/Classes/String+Extension.swift
@@ -36,6 +36,22 @@ public extension String {
         }
         return self.boundingRect(with: toSize, options: option, attributes: attributes, context: nil).size
     }
-    
 }
+// MARK: 自适应获取高度
+public extension String {
+    func AutoGetSize(with font: UIFont, maxWidth width: CGFloat) -> CGSize {
+        let temLable = UILabel(frame: CGRect.init(x: 0, y: 0, width: width, height: 0))
+        temLable.numberOfLines = 0
+        temLable.text = self
+        temLable.font = font
+        temLable.sizeToFit()
+        return temLable.frame.size
+    }
+    func autoLabelHeight(with text:String , labelWidth: CGFloat ,attributes : [NSAttributedStringKey : Any]) -> CGFloat{
+        var size = CGRect()
+        let size2 = CGSize(width: labelWidth, height: 0)//设置label的最大宽度
+        size = text.boundingRect(with: size2, options: NSStringDrawingOptions.usesLineFragmentOrigin, attributes: attributes , context: nil);
+        return size.size.height
+    }
 
+}
